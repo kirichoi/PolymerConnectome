8cc8bf4525861ec88b15ffa0ed2039ff5281287e
Kiri Choi <kirichoi@uw.edu>
2020-09-15 17:30:21 +0900

    Update debye

--- formfactor_AL_debye.py
+++ formfactor_AL_debye.py
@@ -18,7 +18,7 @@ def formfactor(args):
     AL_dist_flat_glo_s = AL_dist_flat_glo_r.reshape((n_glo.value,m_glo.value))
     qrvec = np.logspace(-2,3,100)[args[0]]*spatial.distance.cdist(np.array([AL_dist_flat_glo_s[args[1]]]), AL_dist_flat_glo_s)[0]
     qrvec = qrvec[np.nonzero(qrvec)[0]]
-    ffq = np.sum(np.divide(np.sin(qrvec), qrvec))
+    ffq = np.sum(np.sin(qrvec)/qrvec)
     return ffq
 
 def parallelinit(AL_dist_flat_glo_, n_glo_, m_glo_):
@@ -39,7 +39,7 @@ if __name__ == '__main__':
     n_glo = mp.Value(ctypes.c_int, n)
     m_glo = mp.Value(ctypes.c_int, m)
     
-    paramlist = list(itertools.product(range(1), range(n)))
+    paramlist = list(itertools.product(range(100), range(n)))
     
     pool = mp.Pool(20, initializer=parallelinit, initargs=(AL_dist_flat_glo, n_glo, m_glo))
     
@@ -52,26 +52,17 @@ if __name__ == '__main__':
     
     print(t2-t1)
     
-    # np.save(r'./AL_results.npy', results)
+    np.save(r'./AL_results_debye.npy', results)
     
-    # Pq = np.divide(np.sum(np.array(results).reshape(100, n), axis=1), np.square(n))
+    Pq = np.divide(np.sum(np.divide(np.array(results).reshape(100, n), n), axis=1), n)
     
-    # # fig = plt.figure(figsize=(8,6))
-    # # plt.plot(q_range, Pq, lw=3, color='tab:orange')
-    # # plt.xscale('log')
-    # # plt.xlabel('$q$', fontsize=15)
-    # # plt.ylabel('$P(q)$', fontsize=15)
-    # # plt.tight_layout()
-    # # plt.savefig(r'./AL_form_factor.pdf', dpi=300, bbox_inches='tight')
-    # # plt.show()
-    
-    # fig = plt.figure(figsize=(8,6))
-    # plt.plot(q_range, Pq, lw=3, color='tab:orange')
-    # plt.xscale('log')
-    # plt.yscale('log')
-    # plt.xlabel('$q$', fontsize=15)
-    # plt.ylabel('$P(q)$', fontsize=15)
-    # plt.tight_layout()
-    # plt.savefig(r'./AL_form_factor_log.pdf', dpi=300, bbox_inches='tight')
-    # plt.show()
+    fig = plt.figure(figsize=(8,6))
+    plt.plot(q_range, Pq, lw=3, color='tab:orange')
+    plt.xscale('log')
+    plt.yscale('log')
+    plt.xlabel('$q$', fontsize=15)
+    plt.ylabel('$P(q)$', fontsize=15)
+    plt.tight_layout()
+    plt.savefig(r'./AL_form_factor_debye_log.pdf', dpi=300, bbox_inches='tight')
+    plt.show()
     
